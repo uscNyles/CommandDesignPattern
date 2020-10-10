0787b5b4c839fb7de5f96ece5c1145f91d9e0f10
nylesgeiger <nylesgeiger@10.36.227.4>
2020-10-10 08:53:42 -0400

    Completed the command interface. Completed version 1 of the Agent class.
    Signed-off-by: nylesgeiger <nylesgeiger@10.36.227.4>

--- CommandDesignPatternOct10/src/Agent.java
+++ CommandDesignPatternOct10/src/Agent.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class Agent {
+
+	private ArrayList<Command> orders;
+
+	public Agent() {
+		orders = new ArrayList<Command>();
+	}
+
+	public void placeOrder(Command command) {
+		orders.add(command);
+	}
+
+	public void processOrders() {
+
+		for (Command order : orders) {
+			order.execute();
+		}
+
+	}
 
 }
