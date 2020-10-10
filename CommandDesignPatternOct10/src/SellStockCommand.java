1163171e0c2c5548fc9dbe45ca99f0d5c307200d
cSwiggitySwooty <acolinstuart@gmail.com>
2020-10-10 14:51:24 -0400

    Wrote code for SellStockCommand.java

--- CommandDesignPatternOct10/src/SellStockCommand.java
+++ CommandDesignPatternOct10/src/SellStockCommand.java
@@ -1,4 +1,17 @@
 
-public class SellStockCommand {
+public class SellStockCommand implements Command {
 
+    private StockTrade stockTrade;
+    private String stockName;
+    private int shares;
+
+    public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
+    this.stockTrade = stockTrade;
+    this.stockName = stockName;
+    this.shares = shares;
+    }
+
+    public void execute() {
+        stockTrade.sell(stockName, shares);
+    }
 }
